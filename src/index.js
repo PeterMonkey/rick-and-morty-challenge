c2fd97d7e7c1ca15b3e8f69088dc20dbb6656a6f
Pedro Fernandez <pefernandez1408@gmail.com>
2022-08-13 01:35:40 -0400

    ADD: defining environment variables

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import { setCharacters } from './services/character-services.js'
+
+dotenv.config()
 
 const app = express()
 
 //Data Base conncet
 
 async function main(){
-   await mongoose.connect('mongodb://localhost:27017/test')
-   await setCharacters()
+   await mongoose.connect(process.env.DB_MONGO)
+   //await setCharacters()
    app.listen(3000, () => console.log('Server on port 3000'))
 }
 
